82b81d819851e7ae4a6ebc5f771b0bd6b40f6af0
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-09-10 09:11:56 +0300

    Initial Commit

---
+++ Crazy Time Party Checklist/Crazy_Time_Party_ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crazy_Time_Party_ChecklistApp.swift
+//  Crazy Time Party Checklist
+//
+//  Created by Роман on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Crazy_Time_Party_ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
